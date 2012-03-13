1da1dbd6f6011e303aaedccd4efcc64d5c205eb7
Christopher J. Madsen <perl@cjmweb.net>
2012-03-13 18:58:42 -0500

    Replace backup.sh with backup.pl
      Change createDB.pl to reload the data saved by backup.pl

---
+++ backup.pl
@@ -0,0 +1,56 @@
+#! /usr/bin/perl
+#---------------------------------------------------------------------
+# backup.pl
+# Copyright 2012 Christopher J. Madsen
+#
+# Back up the database to CSV files so it can be stored in Git
+#
+# Only the release data is stored; you have to run recalc.pl to
+# recalculate the Seinfeld chain data.
+#---------------------------------------------------------------------
+
+use strict;
+use warnings;
+use 5.010;
+
+use autodie ':io';
+
+use DBI ();
+use Text::CSV ();
+
+#---------------------------------------------------------------------
+mkdir 'data' unless -d 'data';
+
+my $csv = Text::CSV->new({ binary => 1, eol => "\x0A" })
+    or die "Cannot use CSV: " . Text::CSV->error_diag;
+
+my $db = DBI->connect("dbi:SQLite:dbname=seinfeld.db","","",
+                      { AutoCommit => 1, PrintError => 0, RaiseError => 1 });
+$db->do("PRAGMA foreign_keys = ON");
+
+backup(qw(authors  author_num  author_num author_id));
+backup(qw(dists    dist_id     dist_id dist_name));
+backup(qw(releases release_id  author_num dist_id version date));
+
+$db->disconnect;
+
+#---------------------------------------------------------------------
+sub backup
+{
+  my ($table, $order_by, @fields) = @_;
+
+  open my $fh, ">:utf8", "data/$table.csv";
+
+  $csv->print($fh, \@fields);   # print header row
+
+  my $fields = join(',', @fields);
+
+  my $s = $db->prepare("SELECT $fields FROM $table ORDER BY $order_by");
+  $s->execute;
+
+  while (my $row = $s->fetchrow_arrayref) {
+    $csv->print($fh, $row);
+  }
+
+  close $fh;
+} # end backup
--- createDB.pl
+++ createDB.pl
@@ -3,16 +3,25 @@
 # createDB.pl
 # Copyright 2012 Christopher J. Madsen
 #
-# Create the database
+# Create the database and reload release data
 #---------------------------------------------------------------------
 
 use strict;
 use warnings;
 use 5.010;
 
+use autodie ':io';
+
 use DBI;
-my $db = DBI->connect("dbi:SQLite:dbname=seinfeld.db","","",
+use Text::CSV ();
+
+my $dbname = 'seinfeld.db';
+
+die "$dbname already exists!\n" if -e $dbname;
+
+my $db = DBI->connect("dbi:SQLite:dbname=$dbname","","",
                       { AutoCommit => 0, PrintError => 0, RaiseError => 1 });
+$db->do("PRAGMA foreign_keys = ON");
 
 $db->do(<<'');
 CREATE TABLE authors (
@@ -62,4 +71,38 @@ SELECT release_id, author_id, dist_name, version,
 FROM releases NATURAL JOIN authors NATURAL JOIN dists
 
 $db->commit;
+
+#---------------------------------------------------------------------
+my $csv = Text::CSV->new({ binary => 1, eol => "\x0A" })
+    or die "Cannot use CSV: " . Text::CSV->error_diag;
+
+for my $table (qw(authors dists releases)) {
+  my $fn = "data/$table.csv";
+
+  die "Unable to restore $table: $fn not found\n" unless -e $fn;
+  open my $fh, "<:utf8", $fn;
+
+  say "Loading data from $fn...";
+
+  my $fields = $csv->getline($fh) or die "Can't read header";
+
+  my $field_list   = join(',', @$fields);
+  my $placeholders = join(',', ('?') x @$fields);
+
+  my $insert = $db->prepare(
+    "INSERT INTO $table ($field_list) VALUES ($placeholders)"
+  );
+
+  while (my $row = $csv->getline($fh)) {
+    $insert->execute(@$row);
+  }
+
+  $db->commit;
+
+  close $fh;
+} # end backup
+
+#---------------------------------------------------------------------
+say 'Release data restored.  Now run recalc.pl to find Seinfeld chains.';
+
 $db->disconnect;
